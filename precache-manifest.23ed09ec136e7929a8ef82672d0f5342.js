660a099a1a0e1bc70ca849de687e8903d9cbd286
Darshana <darshana_tak@persistent.com>
2020-02-28 23:41:57 +0530

    Updates

---
+++ precache-manifest.23ed09ec136e7929a8ef82672d0f5342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1501a5a2a1d97eda660a710fd8bd85",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2dec63e2b81805325950",
+    "url": "/monsters-rolodex/static/css/main.cfb99c2e.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/monsters-rolodex/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dec63e2b81805325950",
+    "url": "/monsters-rolodex/static/js/main.8f0635a7.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
